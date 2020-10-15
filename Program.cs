3ac2948937c0be3028ddefa2bd32a350936fff47
Gamaliel Toro <argami@gmail.com>
2020-10-15 14:42:49 +0200

    file watcher, and console app, first commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace FileProcessor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // If a directory is not specified, or directory doesn't exists exit program.
+            if (args.Length != 1 || !Directory.Exists(args[0]))
+            {
+                Console.WriteLine("Usage: Watcher.exe (directory)");
+                return;
+            }
+
+            Watcher.Run(args[0], "*.dat");
+        }
+
+    }
+}
