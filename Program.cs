400e3d11540a5e2ca9dd9f3e586d5cd741573fd7
Gamaliel Toro <argami@gmail.com>
2020-10-15 18:01:27 +0200

    added some of the parts i will be using, but i want to add a DI first and relocate some of the files

--- Program.cs
+++ Program.cs
@@ -1,20 +1,46 @@
 ﻿using System;
 using System.IO;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
 
 namespace FileProcessor
 {
     class Program
     {
+
+        private static ILoggerFactory _loggerFactory;
+        private static ILogger _logger;
+
         static void Main(string[] args)
         {
+            RegisterLoggerFactory();
+
             // If a directory is not specified, or directory doesn't exists exit program.
+            // i will extract this from config later
             if (args.Length != 1 || !Directory.Exists(args[0]))
             {
                 Console.WriteLine("Usage: Watcher.exe (directory)");
                 return;
             }
 
-            Watcher.Run(args[0], "*.dat");
+            _logger = _loggerFactory.CreateLogger<Program>();
+            _logger.LogInformation("Example log message");
+
+            Watcher watcher = new Watcher(_loggerFactory);
+            //filter should be extracted from config
+            watcher.Run(args[0], "*.dat");
+        }
+
+        private static void RegisterLoggerFactory()
+        {
+            _loggerFactory = LoggerFactory.Create(builder =>
+            {
+                builder
+                    .AddFilter("Microsoft", LogLevel.Warning)
+                    .AddFilter("System", LogLevel.Warning)
+                    .AddFilter("LoggingConsoleApp.Program", LogLevel.Debug)
+                    .AddConsole();
+            });
         }
 
     }
--- Watcher.cs
+++ Watcher.cs
@@ -1,14 +1,26 @@
 using System;
 using System.IO;
 using System.Security.Permissions;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
 
 namespace FileProcessor
 {
     public class Watcher
     {
-        [PermissionSet(SecurityAction.Demand, Name = "FullTrust")]
-        public static void Run(string directoryToWatch, string filter)
+
+        private static ILogger _logger;
+
+        public Watcher(ILoggerFactory loggerFactory)
         {
+            _logger = loggerFactory.CreateLogger<Watcher>();
+        }
+
+        [PermissionSet(SecurityAction.Demand, Name = "FullTrust")]
+        public void Run(string directoryToWatch, string filter)
+        {
+
+            _logger.LogInformation("Watching");
             // Create a new FileSystemWatcher and set its properties.
             using (FileSystemWatcher watcher = new FileSystemWatcher())
             {
@@ -25,10 +37,7 @@ namespace FileProcessor
                 watcher.Filter = filter;
 
                 // Add event handlers.
-                watcher.Changed += OnChanged;
                 watcher.Created += OnChanged;
-                watcher.Deleted += OnChanged;
-                watcher.Renamed += OnRenamed;
 
                 // Begin watching.
                 watcher.EnableRaisingEvents = true;
@@ -40,12 +49,10 @@ namespace FileProcessor
         }
 
         // Define the event handlers.
-        private static void OnChanged(object source, FileSystemEventArgs e) =>
+        private static void OnChanged(object source, FileSystemEventArgs e)
+        {
             // Specify what is done when a file is changed, created, or deleted.
             Console.WriteLine($"File: {e.FullPath} {e.ChangeType}");
-
-        private static void OnRenamed(object source, RenamedEventArgs e) =>
-            // Specify what is done when a file is renamed.
-            Console.WriteLine($"File: {e.OldFullPath} renamed to {e.FullPath}");
+        }
     }
 }
